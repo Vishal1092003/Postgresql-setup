7db48ae95f8bb8e54dc296dd68d045658d348881
vishal <codewithme2615@gmail.com>
2025-06-29 18:44:13 +0530

    table creation

--- src/create-table.ts
+++ src/create-table.ts
@@ -1,34 +1,67 @@
+// import { getClient } from "./utils";
+
+// async function createTable() {
+//     const createUserTableQuery = `
+//         CREATE TABLE users (
+//             id SERIAL PRIMARY KEY,
+//             email VARCHAR(255) UNIQUE NOT NULL,
+//             password VARCHAR(255) NOT NULL
+//         );
+//     `;
+
+//     const client = await getClient();
+
+//     await client.query(createUserTableQuery);
+
+//     const createTodosQuery = `
+//         CREATE TABLE todos (
+//             id SERIAL PRIMARY KEY,
+//             title TEXT NOT NULL,
+//             description TEXT,
+//             user_id INTEGER REFERENCES users(id),
+//             done BOOLEAN DEFAULT FALSE
+//         );
+//     `;
+
+
+//     await client.query(createTodosQuery);
+
+//     console.log("Table created successfully!");
+// }
+
+
+
+// createTable();
+
+
 import { getClient } from "./utils";
 
-async function createTable() {
-    const createUserTableQuery = `
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL
-        );
-    `;
-
-    const client = await getClient();
+async function createTable(){
+    const createUserTableQuery=`
+     create table IF NOT EXISTS users(
+      id       serial primary key,
+      email    varchar(255) not null,
+      password varchar(255)  not null
+    )
+    `
+    const client=await getClient();
 
     await client.query(createUserTableQuery);
+    console.log("Users Table create successfully")
 
-    const createTodosQuery = `
-        CREATE TABLE todos (
-            id SERIAL PRIMARY KEY,
-            title TEXT NOT NULL,
-            description TEXT,
-            user_id INTEGER REFERENCES users(id),
-            done BOOLEAN DEFAULT FALSE
-        );
-    `;
-
-
+    const createTodosQuery=`
+    create table if not exists todos(
+       id          serial primary key,
+     title       text not null,
+     description  text,
+     user_id       integer references users(id),
+     done     boolean default false
+    )
+    
+    `
     await client.query(createTodosQuery);
 
-    console.log("Table created successfully!");
+  console.log("Todos Table create successfully");
 }
-
-
 
 createTable();
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,22 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const uri ="postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg";
+
+    const client = new Client({
+        host:"localhost",
+        user:"postgres",
+        password:"Suarsapv@123",
+        port:5432,
+        database:"TEST-Postgresql"
+    });
     await client.connect();
     return client;
 }
+getClient()
+.then((client)=>{
+    console.log(`db connected to ${client.database} on port number ${client.database} on host ${client.host}`)
+})
+.catch((error)=>{
+  console.log("error while connecting to the db ",error);
+})
